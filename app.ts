8a512230164456500eced86dd4cd06272e4e9892
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-05-21 01:37:33 +0900

    feat: DB 연결
    - Sequelize 연결

--- app.ts
+++ app.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import userRouter from './route/user-router';
-
+import db from './models'
 const app = express();
 
 app.use('api/', userRouter);
 
-app.listen();
-
+app.listen(async () => {
+    try{
+        await db.sequelize.authenticate()
+        .then(() => {
+            console.log("DB connection success")
+        })
+        console.log(`server is running on ${process.env.ORIGIN}:${process.env.PORT}`)
+    }
+    catch (err) {
+        console.error(err);
+        console.log("Server running failed")
+    }
+});
 console.log('서버가 실행 중입니다...');
---
+++ models/index.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from "sequelize"
+import config from "../config/config.json"
+
+
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: 'mysql',
+    define: { timestamps: false},
+    timezone: "+09:00",  
+    dialectOptions: { connectTimeout: 150000 },
+    pool: { 
+      max: 30000, 
+      min: 0, acquire: 30000, idle: 10000 }
+  }
+
+);
+
+
+const db = { sequelize:sequelize, Sequelize }
+
+export default db;
