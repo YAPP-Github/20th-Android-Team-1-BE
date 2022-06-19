595974cb15edf743a4ee3215e1d06b5ff729cee3
Subin Min <tnqls5417@naver.com>
2022-06-19 19:22:52 +0900

    fix: 에러 픽스
    - 컴마 제거 후 prettier 적용

--- services/promising-service.ts
+++ services/promising-service.ts
@@ -1,30 +1,28 @@
-import PromisingModel from "../models/promising";
-import CategoryKeyword from "../models/category-keyword";
-import arrayUtil from "../utils/array";
-import { NotFoundError, InternalServerError } from "routing-controllers";
-import User from "../models/user";
+import PromisingModel from '../models/promising';
+import CategoryKeyword from '../models/category-keyword';
+import arrayUtil from '../utils/array';
+import { NotFoundError, InternalServerError } from 'routing-controllers';
+import User from '../models/user';
 
 class PromisingService {
-    async create(promisingInfo: any) {
-        try {
-            const category = await CategoryKeyword.findOne({ where: { id: promisingInfo.categoryId } })
-            const user = await User.findOne({ where: { id: promisingInfo.ownerId } })
-            if (!user) return new NotFoundError('User Not Found')
-            if (!category) return new NotFoundError('CategoryKeyword Not Found')
-            promisingInfo.category = category;
-            promisingInfo = await arrayUtil.deleteJsonKey(promisingInfo, 'categoryId')
+  async create(promisingInfo: any) {
+    try {
+      const category = await CategoryKeyword.findOne({ where: { id: promisingInfo.categoryId } });
+      const user = await User.findOne({ where: { id: promisingInfo.ownerId } });
+      if (!user) return new NotFoundError('User Not Found');
+      if (!category) return new NotFoundError('CategoryKeyword Not Found');
+      promisingInfo.category = category;
+      promisingInfo = await arrayUtil.deleteJsonKey(promisingInfo, 'categoryId');
 
-            const promising = new PromisingModel(promisingInfo);
-            await promising.save()
+      const promising = new PromisingModel(promisingInfo);
+      await promising.save();
 
-            return promisingInfo;
-        } catch (err: any) {
-            console.log(err)
-            return new InternalServerError('Internal Server Error')
-        }
-    },
-};
+      return promisingInfo;
+    } catch (err: any) {
+      console.log(err);
+      return new InternalServerError('Internal Server Error');
+    }
+  }
+}
 
 export default new PromisingService();
-
-
