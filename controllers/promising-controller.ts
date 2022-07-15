7d9ad6015fecc784a6e3ffd86507026faba32308
Subin Min <tnqls5417@naver.com>
2022-07-16 01:16:03 +0900

    feat: 카테고리별 랜덤 이름 조회 API 구현
    - 서비스 구현
    - controller단에서 JSON 접근
    - Swagger 문서 작성

--- controllers/promising-controller.ts
+++ controllers/promising-controller.ts
@@ -7,12 +7,13 @@ import { TimeRequest } from '../dtos/time/request';
 import { CreatedPromisingResponse } from '../dtos/promising/response';
 import { ValidationException } from '../utils/error';
 import categoryService from '../services/category-service';
-import { CategoryResponse } from '../dtos/category/response';
+import { CategoryResponse, RandomNameResponse } from '../dtos/category/response';
 import { BadRequestException } from '../utils/error';
 import timeUtil from '../utils/time';
 import promisingDateService from '../services/promising-date-service';
 import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
 import { PromiseResponse } from '../dtos/promise/response';
+import randomName from '../constants/category-name.json';
 
 @OpenAPI({ security: [{ bearerAuth: [] }] })
 @JsonController('/promisings')
@@ -129,6 +130,29 @@ class PromisingController {
   async getPromisingCategories(@Res() res: Response) {
     const categories = await categoryService.getAll();
     return res.status(200).send(categories.map((category) => new CategoryResponse(category)));
+  }
+
+  @OpenAPI({
+    summary: 'Get random Promising name by categoryId',
+    description: 'Category by categoryId must exist.'
+  })
+  @ResponseSchema(RandomNameResponse)
+  @Get('/categories/:categoryId/name')
+  @UseBefore(UserAuthMiddleware)
+  async getPromisingRandomNameByCategory(
+    @Param('categoryId') categoryId: number,
+    @Res() res: Response
+  ) {
+    const category = await categoryService.getOneById(categoryId);
+    const names = randomName[category.id];
+    if (!names)
+      throw new BadRequestException(
+        'categoryId',
+        'random names with requested categoryId not found'
+      );
+
+    const randomIdx = Math.floor(Math.random() * names.length);
+    return res.status(200).send(new RandomNameResponse(randomName[category.id][randomIdx]));
   }
 }
 
