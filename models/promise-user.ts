8a84c79098c92af75e1a3c39ca6d10a8d1a9fdde
cpplovelove <gsgslolo@naver.com>
2022-08-01 00:12:55 +0900

    bug: 탈퇴 기능 에러 픽스
    - Promise_user 테이블 유니크키 설정 변경

--- models/promise-user.ts
+++ models/promise-user.ts
@@ -1,9 +1,14 @@
-import { Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, ForeignKey, Model, Table, PrimaryKey, AutoIncrement } from 'sequelize-typescript';
 import PromiseModel from './promise';
 import User from './user';
 
 @Table({ tableName: 'Promise_User', modelName: 'PromiseUser' })
 class PromiseUser extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column({ type: DataType.INTEGER, field: 'promiseUserId' })
+  id: number;
+
   @ForeignKey(() => User)
   @Column({ type: DataType.BIGINT })
   userId: number;
@@ -12,5 +17,5 @@ class PromiseUser extends Model {
   @Column({ type: DataType.INTEGER })
   promiseId: number;
 }
-
+ 
 export default PromiseUser;
